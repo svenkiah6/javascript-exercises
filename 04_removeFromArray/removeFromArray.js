0e11d1a4449d5921f98db05c9c9b9f30b36a3575
Ashish <113494198+svenkiah6@users.noreply.github.com>
2022-12-05 11:16:51 +0400

    Add solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,30 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, toRemove, ...moreToRemove) {
+
+
+
+
+    //moreToRemove is an empty array by default
+
+        moreToRemove.push(toRemove);
+
+
+        for(let i=0; i<moreToRemove.length;i++)
+        {
+            let index = myArray.indexOf(moreToRemove[i])
+
+            if(index>-1)
+            {
+                myArray.splice(index,1);
+            }
+        
+
+        }
+
+
+
+       
+
+    return myArray;
 
 };
 
