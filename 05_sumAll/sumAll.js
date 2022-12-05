f50d9f22a2c5dc15ad1e83bcb3f7593dd813d8ee
Ashish <113494198+svenkiah6@users.noreply.github.com>
2022-12-05 11:16:24 +0400

    Add solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,38 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+
+
+
+    if(typeof(num1) != "number" || typeof(num2) != "number")
+    {
+        return "ERROR";
+    }
+
+    if(num1<0 || num2<0 )
+    {
+
+        return "ERROR";
+    }
+
+
+
+    let start = num1;
+    let end = num2;
+    let sum = 0;
+
+    if(num1>num2)
+    {
+        start =num2;
+         end=num1;  
+    }
+
+   
+    for(i=start;i<=end;i++)
+    {
+        sum+=i;
+    }
+
+    return sum;
 
 };
 
